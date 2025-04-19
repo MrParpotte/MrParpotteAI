8f10f100b8257851d4a1204bc3e7d448278ddf0a
MrParpotte <hugosalvatori100@gmail.com>
2025-04-19 11:29:22 +0200

    qsd

--- deploy-commands.js
+++ deploy-commands.js
@@ -13,6 +13,24 @@ for (const file of commandFiles) {
   commands.push(command.data.toJSON());
 }
 
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  if ('data' in command && 'execute' in command) {
+    commands.push(command.data.toJSON());
+  } else {
+    console.warn(`[WARNING] La commande ${filePath} est mal formée`);
+  }
+}
+
+const GUILD_ID = '1281283774864429117';
+
+await rest.put(
+  Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
+  { body: commands },
+);
+
+
 const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 
 (async () => {
