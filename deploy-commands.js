5a37146374ef263105d99f0f98533b0e001bd268
MrParpotte <hugosalvatori100@gmail.com>
2025-04-19 11:33:57 +0200

    qsd

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,45 +1,27 @@
-require('dotenv').config();
 const { REST, Routes } = require('discord.js');
 const fs = require('node:fs');
-const path = require('node:path');
+require('dotenv').config();
 
 const commands = [];
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-  const filePath = path.join(commandsPath, file);
-  const command = require(filePath);
+  const command = require(`./commands/${file}`);
   commands.push(command.data.toJSON());
 }
-
-for (const file of commandFiles) {
-  const filePath = path.join(commandsPath, file);
-  const command = require(filePath);
-  if ('data' in command && 'execute' in command) {
-    commands.push(command.data.toJSON());
-  } else {
-    console.warn(`[WARNING] La commande ${filePath} est mal formée`);
-  }
-}
-
-const GUILD_ID = '1281283774864429117';
-
-await rest.put(
-  Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
-  { body: commands },
-);
-
 
 const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 
 (async () => {
   try {
     console.log('🛠️ Déploiement des commandes Slash...');
+
     await rest.put(
       Routes.applicationCommands(process.env.CLIENT_ID),
+      // ou Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID) pour test immédiat
       { body: commands },
     );
+
     console.log('✅ Déploiement terminé !');
   } catch (error) {
     console.error(error);
